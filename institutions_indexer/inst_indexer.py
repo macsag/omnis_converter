364105c426e9807688966014821df9aeeded20fd
macsag <maciejsagata@gmail.com>
2019-10-23 12:13:46 +0200

    added indexing by es_id

--- institutions_indexer/inst_indexer.py
+++ institutions_indexer/inst_indexer.py
@@ -9,15 +9,17 @@ def read_json_file(file_in):
             yield line
 
 
-def index_libraries(library, library_index: dict):
+def index_libraries(library, library_index_mak: dict, library_index_es: dict):
     lib_as_dict = json.loads(library)
     met_orig = lib_as_dict['_source'].get('metadata_original')
     if met_orig and 'makplus' in met_orig:
-        library_index.setdefault(lib_as_dict['_source']['code'], Library(lib_as_dict))
+        library_index_mak.setdefault(lib_as_dict['_source']['code'], Library(lib_as_dict))
+        library_index_es.setdefault(lib_as_dict['_id'], Library(lib_as_dict))
 
 
-def create_index(file_in):
-    lib_ind = {}
+def create_lib_indexes(file_in):
+    lib_ind_mak = {}
+    lib_in_es = {}
     for rcd in read_json_file(file_in):
-        index_libraries(rcd, lib_ind)
-    return lib_ind
+        index_libraries(rcd, lib_ind_mak, lib_in_es)
+    return lib_ind_mak, lib_in_es
