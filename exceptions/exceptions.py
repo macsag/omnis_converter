7f85233be0bb36ea03c766a53ff034db7089c5f3
macsag <maciejsagata@gmail.com>
2019-10-30 23:07:20 +0100

    new exception added

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -12,6 +12,11 @@ class No245FieldFoundOrTooMany245Fields(OmnisConverterException):
         return 'Invalid record - no 245 field or too many 245 fields.'
 
 
+class No008FieldFound(OmnisConverterException):
+    def __str__(self):
+        return 'Invalid record - no 245 field or too many 245 fields.'
+
+
 class DescriptorNotResolved(OmnisConverterException):
     def __str__(self):
         return 'Invalid record - descriptor not resolved.'
