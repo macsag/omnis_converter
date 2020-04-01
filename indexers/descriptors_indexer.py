63266f1d33b114cfa004d2b72200afc53c8267f3
macsag <maciejsagata@gmail.com>
2020-04-01 13:10:37 +0200

    switch from json to ujson

--- indexers/descriptors_indexer.py
+++ indexers/descriptors_indexer.py
@@ -1,4 +1,4 @@
-import json
+import ujson
 import os
 
 from tqdm import tqdm
@@ -7,17 +7,17 @@ from commons.marc_iso_commons import prepare_name_for_indexing
 
 
 def index_descriptors(path_dir):
-    descr_dict = {}
+    descriptors_dict = {}
 
     for filename in os.listdir(path_dir):
         path_file = os.sep.join([path_dir, filename])
         with open(path_file, 'r', encoding='utf-8') as fp:
             for line in tqdm(fp):
-                line_as_dict = json.loads(line)
+                line_as_dict = ujson.loads(line)
                 index = line_as_dict.get('_index')
                 es_id = line_as_dict.get('_id')
                 name = line_as_dict.get('_source')['descr_name']
                 indexing_name = prepare_name_for_indexing(name)
-                descr_dict.setdefault(indexing_name, (es_id, index, name))
+                descriptors_dict.setdefault(indexing_name, (es_id, index, name))
 
-    return descr_dict
+    return descriptors_dict
